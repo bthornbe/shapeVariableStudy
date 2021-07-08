6bc73ec22bd52227d82164800639068d39038d08
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-08 12:32:20 -0400

    trying to plot C parameter

--- sphericity.py
+++ sphericity.py
@@ -16,7 +16,8 @@ fnames = [
 ]
 print("c")
 dFrames = {}
-hists = {}
+cHists = {}
+dHists = {}
 
 for fname in fnames:
     fullname = "root://cmsxrootd.fnal.gov/"+floc+fname
@@ -47,8 +48,11 @@ for mass in dFrames:
     filteredFrame = filteredFrame.Define("denominator", "double denom=0; for (int i=0; i<nTracks; i++) denom += sqrt(Tracks[i].Mag2()); return denom;")
     filteredFrame = filteredFrame.Define("sphericityTensor", "TMatrixD s(3,3); TArrayD array(9); for (int i=0; i<9; i++) array[i]=0; s.SetMatrixArray(array.GetArray()); for (int i=0; i<nTracks; i++) { for (int j=0; j<3; j++) {for (int k=0; k<3; k++) {s[j][k]+= (momenta[i][j]*momenta[i][k]/(sqrt(Tracks[i].Mag2())*denominator));}}} return s;")
     filteredFrame = filteredFrame.Define("eigenVals", "TMatrixDEigen eigen(sphericityTensor); return eigen.GetEigenValues();")
+    filteredFrame = filteredFrame.Define("C", "return 3*(eigenVals[0]*eigenVals[1]+eigenVals[0]*eigenVals[2]+eigenVals[1]*eigenVals[2]);")
+    filteredFrame = filteredFrame.Define("D","return 27*eigenVals[0]*eigenVals[1]*eigenVals[2];")
     print("d4")
-    #hists[mass] = filteredFrame.Histo1D(("filteredTracks"+mass,mass, 50, 0., 500.), "filteredTracks")
+    cHists[mass] = filteredFrame.Histo1D(("C"+mass,mass, 50, 0., 1.), "C")
+    dHists[mass] = filteredFrame.Histo1D(("D" + mass, mass, 50, 0., 1.), "D")
     print("d5")
 print("e")
 #can I put this before the loop so I can combine the loops?
@@ -58,31 +62,24 @@ print("f")
 #copied from plotHelpers:
 can = ROOT.TCanvas("canName", "canTitle")
 
-h_keys = hists.keys()
-h_values = hists.values()
-
-maxy = 1.5 * 1e4 * max(hists[mass].GetMaximum() for mass in hists)
-miny = 1e-1
-
 i = 0
 more_colors = [ROOT.kAzure+2, ROOT.kGreen+2, ROOT.kPink+4, ROOT.kOrange+10, ROOT.kOrange, ROOT.kSpring+7]
-for mass in hists:
-    hists[mass].SetLineColor(more_colors[i])
-    hists[mass].SetMarkerColor(more_colors[i])
+for mass in cHists:
+    cHists[mass].SetLineColor(more_colors[i])
+    cHists[mass].SetMarkerColor(more_colors[i])
     i += 1
     i %= len(more_colors)
 
-hists["mMed-125"].SetMinimum(miny)
-hists["mMed-125"].SetMaximum(maxy)
-hists["mMed-125"].Draw("hist")
-ROOT.gPad.SetLogy(1)
-hists["mMed-400"].Draw("same")
-hists["mMed-750"].Draw("same")
-hists["mMed-1000"].Draw("same")
+cHists["mMed-125"].SetMinimum(0)
+cHists["mMed-125"].SetMaximum(1)
+cHists["mMed-125"].Draw("hist")
+cHists["mMed-400"].Draw("same")
+cHists["mMed-750"].Draw("same")
+cHists["mMed-1000"].Draw("same")
 
 leg = ROOT.TLegend(.66, .64, .8, .88)
-for mass in hists.keys():
-    leg.AddEntry(hists[mass], mass, "l")
+for mass in cHists.keys():
+    leg.AddEntry(cHists[mass], mass, "l")
 leg.Draw()
 
-can.SaveAs("rtestplot.pdf")
+can.SaveAs("sphericity.pdf")
