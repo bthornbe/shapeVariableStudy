71623b809382057a659d6215f321bb7716c2a746
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-13 15:04:06 -0400

    seeing if any of the defined columns work

--- sphericity.py
+++ sphericity.py
@@ -40,7 +40,7 @@ for fname in fnames:
         .Define("CutHT", "double cutht=0; for (int i=0; i<Jets.size(); i++) if (Jets[i].Pt()>30 and abs(Jets[i].eta())<2.4) cutht+=Jets[i].Pt(); return cutht") \
         .Filter("CutHT>500") \
         .Define("Momenta", "vector<vector<double>> p; for (int i=0; i<nTracks; i++) {p[i].push_back(Tracks[i].x()); p[i].push_back(Tracks[i].y()); p[i].push_back(Tracks[i].z());} return p;") \
-        .Define("Val", "return (1)") \
+        .Define("Val", "return (Momenta[0][0])") \
         .Define("Denominator", "double denom=0; for (int i=0; i<nTracks; i++) denom += sqrt(Tracks[i].Mag2()); return denom;") \
         .Define("SphericityTensor", "TMatrixDSym s(3,3); TArrayD array(9); for (int i=0; i<9; i++) array[i]=0; s.SetMatrixArray(array.GetArray()); for (int i=0; i<nTracks; i++) { for (int j=0; j<3; j++) {for (int k=0; k<3; k++) {s[j][k]+= (Momenta[i][j]*Momenta[i][k]/(sqrt(Tracks[i].Mag2())*Denominator));}}} return s;") \
         .Define("EigenVals", "TMatrixDSymEigen eigen(SphericityTensor); return eigen.GetEigenValues();") \
@@ -52,7 +52,7 @@ for fname in fnames:
     #cHists[mass] = filteredFrames[mass].Histo1D(models[mass+"C"], "C").Clone("cloneC"+mass)
     models[mass + "D"] = ROOT.RDF.TH1DModel("D" + mass, mass, 50, 0., 1.)
     #dHists[mass] = filteredFrames[mass].Histo1D(models[mass + "D"], "D").Clone("cloneD"+mass)
-    cHists[mass] =filteredFrames[mass].Histo1D(models[mass + "C"], "Val").Clone("cloneC"+mass)
+    cHists[mass] =filteredFrames[mass].Histo1D(models[mass + "C"], "cutHT").Clone("cloneC"+mass)
     print("d5")
 print("e")
 
