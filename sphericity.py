05d3159927cbb13db2476a55289394cd1c97c2b6
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-13 12:14:08 -0400

    trying Denominator

--- sphericity.py
+++ sphericity.py
@@ -51,7 +51,7 @@ for fname in fnames:
     #cHists[mass] = filteredFrames[mass].Histo1D(models[mass+"C"], "C").Clone("cloneC"+mass)
     models[mass + "D"] = ROOT.RDF.TH1DModel("D" + mass, mass, 50, 0., 1.)
     #dHists[mass] = filteredFrames[mass].Histo1D(models[mass + "D"], "D").Clone("cloneD"+mass)
-    cHists[mass] =filteredFrames[mass].Histo1D(models[mass + "C"], "Momenta [0][0]").Clone("cloneC"+mass)
+    cHists[mass] =filteredFrames[mass].Histo1D(models[mass + "C"], "Denominator").Clone("cloneC"+mass)
     print("d5")
 print("e")
 
