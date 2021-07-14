1443570739f5285038c4e69ab341ccf1ee2017b1
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-14 11:18:46 -0400

    maybe a bug fix?

--- reproducer.py
+++ reproducer.py
@@ -12,7 +12,7 @@ dFrame = ROOT.ROOT.RDataFrame(tname, fullname).Define("nTracks", "Tracks.size()"
             "vector<vector<double>> p; for (int i=0; i<nTracks; i++) {p[i].push_back(Tracks[i].x()); p[i].push_back(Tracks[i].y()); p[i].push_back(Tracks[i].z());} return p;") \
     .Define("Val", "return (Momenta[0][0])")
 
-model = ROOT.RDF.TH1DModel("Val", "Val", 50, 0., 1000.)
+model = ROOT.RDF.TH1DModel("Val", "Val", 50, -1000., 1000.)
 hist = dFrame.Histo1D(model, "Val")
 
 can = ROOT.TCanvas("canName", "canTitle")
