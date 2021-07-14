5256509ba3a3f72e337974f4bae2f44c34b6104e
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-14 12:52:22 -0400

    plotting an actual shape variable! Will it work?

--- sphericity.py
+++ sphericity.py
@@ -48,11 +48,11 @@ for fname in fnames:
         .Define("D","return 27*EigenVals[0]*EigenVals[1]*EigenVals[2];")
     #print(filteredFrames[mass].Count().GetValue())
     print("d4")
-    models[mass+"C"] = ROOT.RDF.TH1DModel("C"+mass, mass, 50, -1000., 1000.)
+    models[mass+"C"] = ROOT.RDF.TH1DModel("C"+mass, mass, 50, 0., 1.)
     #cHists[mass] = filteredFrames[mass].Histo1D(models[mass+"C"], "C").Clone("cloneC"+mass)
     models[mass + "D"] = ROOT.RDF.TH1DModel("D" + mass, mass, 50, 0., 1.)
     #dHists[mass] = filteredFrames[mass].Histo1D(models[mass + "D"], "D").Clone("cloneD"+mass)
-    cHists[mass] =filteredFrames[mass].Histo1D(models[mass+"C"], "Val").Clone("cloneC"+mass)
+    cHists[mass] =filteredFrames[mass].Histo1D(models[mass+"C"], "C").Clone("cloneC"+mass)
     print("d5")
 print("e")
 
