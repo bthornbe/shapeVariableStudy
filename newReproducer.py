f566a3d1c51679af701b673b61dba06ad5a1c666
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-15 12:54:34 -0400

    undoing last edit

--- newReproducer.py
+++ newReproducer.py
@@ -6,7 +6,7 @@ tname = "TreeMaker2/PreSelection"
 
 mystring= \
 '''
-TArrayD array(9,[0,0,0,0,0,0,0,0,0]);   
+TArrayD array(9);   
 for (int i=0; i<nTracks; i++) { 
     for (int j=0; j<3; j++) {
         for (int k=0; k<3; k++) {
