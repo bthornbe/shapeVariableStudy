fd786cea7d9ba768e49b23ca123f1384d1c505d0
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-15 13:44:47 -0400

    making SphericityTensor a 2d array and converting it to a TMatrixDSym in the Define() for EigenVals

--- sphericity.py
+++ sphericity.py
@@ -27,6 +27,29 @@ models = {}
 
 trackPtCut=1
 
+tensorString = \
+    '''
+    vector<vector<double>> s(3, vector<int>(3, 0));   
+    for (int i=0; i<nTracks; i++) 
+    { 
+        for (int j=0; j<3; j++) 
+        {
+            for (int k=0; k<3; k++) 
+            {
+                s.at(j).at(k) += Momenta.at(i).at(j)*Momenta.at(i).at(k)/(sqrt(Tracks[i].Mag2())*Denominator);
+            }
+        }
+    } 
+    return s;
+    '''
+
+eigenString = \
+'''
+double* a = SphericityTensor.data()
+TMatrixDSym s(3, a);
+TMatrixDSymEigen eigen(s); return eigen.GetEigenValues();
+'''
+
 for fname in fnames:
     fullname = "root://cmsxrootd.fnal.gov/"+floc+fname
     tname = "TreeMaker2/PreSelection"
@@ -34,19 +57,7 @@ for fname in fnames:
     dFrames[mass] = ROOT.ROOT.RDataFrame(tname, fullname)
     print("d1")
     print (dFrames[mass].Count().GetValue())
-    mystring= \
-    '''
-    TArrayD array(9);   
-    for (int i=0; i<nTracks; i++) { 
-        for (int j=0; j<3; j++) {
-            for (int k=0; k<3; k++) {
-                array[j+3*k] += Momenta.at(i).at(j)*Momenta.at(i).at(k)/(sqrt(Tracks[i].Mag2())*Denominator);
-            }
-        }
-    } 
-    TMatrixDSym s(3, array.GetArray());
-    return s;
-    '''
+
     # it's more efficient to define ntracks before the loop, right?
     #find the HT the detector "sees" so that we can cut on that for l1 trigger:
     filteredFrames[mass]=dFrames[mass].Define("nTracks", "Tracks.size()") \
@@ -55,9 +66,9 @@ for fname in fnames:
         .Filter("CutHT>500") \
         .Define("Momenta", "vector<vector<double>> p; for (int i=0; i<nTracks; i++) {p.emplace_back(); p[i].push_back(Tracks[i].x()); p[i].push_back(Tracks[i].y()); p[i].push_back(Tracks[i].z());} return p;") \
         .Define("Denominator", "double denom=0; for (int i=0; i<nTracks; i++) denom += sqrt(Tracks[i].Mag2()); return denom;") \
-        .Define("SphericityTensor", mystring) \
+        .Define("SphericityTensor", tensorString) \
         .Define("Val", "return (SphericityTensor[0][0])") \
-        .Define("EigenVals", "TMatrixDSymEigen eigen(SphericityTensor); return eigen.GetEigenValues();") \
+        .Define("EigenVals", eigenString) \
         .Define("C", "return (3*(EigenVals[0]*EigenVals[1]+EigenVals[0]*EigenVals[2]+EigenVals[1]*EigenVals[2]));") \
         .Define("D","return 27*EigenVals[0]*EigenVals[1]*EigenVals[2];") # the nTracks cut is probably unnecessary
     #print(filteredFrames[mass].Count().GetValue())
