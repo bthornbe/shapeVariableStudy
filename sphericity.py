482b1b041230d935f61ffd110ff10446d535aed4
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-20 15:49:53 -0400

    FINALLY plotting sphericity

--- sphericity.py
+++ sphericity.py
@@ -16,7 +16,7 @@ fnames = [
 
 dFrames = {}
 filteredFrames = {}
-cHists = {}
+sHists = {}
 dHists = {}
 lHists = {}
 sHists = {}
@@ -95,64 +95,28 @@ for fname in fnames:
         .Define("SphericityTensor2", tensorString2) \
         .Define("EigenVals2",eigenString2) \
         .Define("Sphericity", "return (EigenVals2 [1] + EigenVals2 [2])*3/2")# the nTracks cut is probably unnecessary
-    #print(filteredFrames[mass].Count().GetValue())
-    print("d4")
-    models[mass+"C"] = ROOT.RDF.TH1DModel("C"+mass, mass, 50, 0., 1.)
-    cHists[mass] = filteredFrames[mass].Histo1D(models[mass+"C"], "C").Clone("cloneC"+mass)
-    models[mass + "D"] = ROOT.RDF.TH1DModel("D" + mass, mass, 50, 0., 1.)
-    dHists[mass] = filteredFrames[mass].Histo1D(models[mass + "D"], "D").Clone("cloneD"+mass)
-    models[mass + "L"] = ROOT.RDF.TH1DModel("L" + mass, mass, 50, 0., 1.)
-    lHists[mass] = filteredFrames[mass].Histo1D(models[mass + "L"], "LambdaMax").Clone("cloneL" + mass)
+
     models[mass + "S"] = ROOT.RDF.TH1DModel("S" + mass, mass, 50, 0., 1.)
     sHists[mass] = filteredFrames[mass].Histo1D(models[mass + "S"], "Sphericity").Clone("cloneS" + mass)
 
-    print("d5")
-print("e")
-
-
-print("f")
-print(cHists.keys())
-
-#copied from plotHelpers:
 can = ROOT.TCanvas("canName", "canTitle")
-print("g")
-file = ROOT.TFile('hists', 'RECREATE')
-print("g1")
-cHists["mMed-125"].Write()
-print("g2")
-'''
-i = 0
-more_colors = [ROOT.kAzure+2, ROOT.kGreen+2, ROOT.kPink+4, ROOT.kOrange+10, ROOT.kOrange, ROOT.kSpring+7]
-print("h")
-for mass in cHists.keys():
-    print("i")
-    cHists[mass].SetLineColor(more_colors[i])
-    print("j")
-    cHists[mass].SetMarkerColor(more_colors[i])
-    print("k")
-    i += 1
-    print("l")
-    i %= len(more_colors)'''
 
-cHists["mMed-125"].SetLineColor(2)
-cHists["mMed-400"].SetLineColor(3)
-cHists["mMed-750"].SetLineColor(4)
-cHists["mMed-1000"].SetLineColor(6)
+sHists["mMed-125"].SetLineColor(2)
+sHists["mMed-400"].SetLineColor(3)
+sHists["mMed-750"].SetLineColor(4)
+sHists["mMed-1000"].SetLineColor(6)
 print("m")
 
-#cHists["mMed-125"].SetMinimum(0)
-cHists["mMed-125"].SetMaximum(1300)
-cHists["mMed-125"].Draw("hist")
-cHists["mMed-400"].Draw("same")
-cHists["mMed-750"].Draw("same")
-cHists["mMed-1000"].Draw("same")
-
-print("n")
+sHists["mMed-125"].SetMaximum(1300)
+sHists["mMed-125"].Draw("hist")
+sHists["mMed-400"].Draw("same")
+sHists["mMed-750"].Draw("same")
+sHists["mMed-1000"].Draw("same")
 
 leg = ROOT.TLegend(.66, .64, .8, .88)
-print("o")
-for mass in cHists.keys():
-    leg.AddEntry(cHists[mass], mass, "l")
+
+for mass in sHists.keys():
+    leg.AddEntry(sHists[mass], mass, "l")
 leg.Draw()
 print("p")
-can.SaveAs("shapesPlot.pdf")
+can.SaveAs("sphericityPlot.pdf")
