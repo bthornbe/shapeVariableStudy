c70cf4a7e1cf70c059636af6be541827e21f4902
Ben Thornberry <bthornbe@vols.utk.edu>
2021-07-20 12:34:56 -0400

    made cParameter

--- cParameter.py
+++ cParameter.py
@@ -1,4 +1,4 @@
-# make a histogram of shape variables that use the sphericity tensor
+# make a histogram of the C parameter
 
 import ROOT
 import cms_style
@@ -27,13 +27,13 @@ trackPtCut = 1
 tensorString = \
     '''
     vector<vector<double>> s{{0,0,0},{0,0,0},{0,0,0}};    
-    for (int i=0; i<nTracks; i++) 
+    for (int i=0; i<nPassingTracks; i++) 
     { 
         for (int j=0; j<3; j++) 
         {
             for (int k=0; k<3; k++) 
             {
-                s.at(j).at(k) += Momenta.at(i).at(j)*Momenta.at(i).at(k)/(sqrt(Tracks[i].Mag2())*Denominator);
+                s.at(j).at(k) += Momenta.at(i).at(j)*Momenta.at(i).at(k)/(sqrt(PassingTracks[i].Mag2())*Denominator);
             }
         }
     } 
@@ -61,22 +61,37 @@ for (int i = 0; i < 3; i++)
 return eigenVals2;
 '''
 
+trackPtCut = 1
+passTrackString = \
+'''
+auto passTracks = Tracks; 
+passTracks.clear(); 
+for (int i = 0; i < nTracks; i ++) 
+{
+    if (Tracks[i].Perp2() > ''' + str(trackPtCut**2) + ''' && abs(Tracks[i].Eta()) < 2.5 && Tracks_fromPV0[i] >=2  && Tracks_matchedToPFCandidate[i]) 
+        passTracks.push_back(Tracks[i]);
+} 
+return passTracks;
+'''
+
 for fname in fnames:
     fullname = "root://cmsxrootd.fnal.gov/"+floc+fname
     tname = "TreeMaker2/PreSelection"
     mass = fname.split("_")[2]
     dFrames[mass] = ROOT.ROOT.RDataFrame(tname, fullname)
 
-    #find the HT the detector "sees" so that we can cut on that for l1 trigger:
     filteredFrames[mass]=dFrames[mass].Define("nTracks", "Tracks.size()") \
         .Filter("nTracks > 0") \
-        .Define("CutHT", "double cutht=0; for (int i=0; i<Jets.size(); i++) if (Jets[i].Pt()>30 and abs(Jets[i].eta())<2.4) cutht+=Jets[i].Pt(); return cutht") \
+        .Define("CutHT",
+                "double cutht=0; for (int i=0; i<Jets.size(); i++) if (Jets[i].Pt()>30 and abs(Jets[i].eta())<2.4) cutht+=Jets[i].Pt(); return cutht") \
         .Filter("CutHT>500") \
-    #cut on tracks
-        .Define("Momenta", "vector<vector<double>> p; for (int i=0; i<nTracks; i++) {p.emplace_back(); p[i].push_back(Tracks[i].x()); p[i].push_back(Tracks[i].y()); p[i].push_back(Tracks[i].z());} return p;") \
-        .Define("Denominator", "double denom=0; for (int i=0; i<nTracks; i++) denom += sqrt(Tracks[i].Mag2()); return denom;") \
+        .Define("PassingTracks", passTrackString) \
+        .Define("nPassingTracks", "PassingTracks.size()") \
+        .Define("Momenta",
+                "vector<vector<double>> p; for (int i=0; i<nPassingTracks; i++) {p.emplace_back(); p[i].push_back(PassingTracks[i].x()); p[i].push_back(PassingTracks[i].y()); p[i].push_back(PassingTracks[i].z());} return p;") \
+        .Define("Denominator", "double denom=0; for (int i=0; i<nPassingTracks; i++) denom += sqrt(PassingTracks[i].Mag2()); return denom;") \
         .Define("SphericityTensor1", tensorString) \
-        .Define("Denominator2", "double denom=0; for (int i=0; i<nTracks; i++) denom += Tracks[i].Mag2(); return denom;") \
+        .Define("Denominator2", "double denom=0; for (int i=0; i<nPassingTracks; i++) denom += PassingTracks[i].Mag2(); return denom;") \
         .Define("SphericityTensor2", otherTensorString) \
         .Define("EigenVals", eigenString) \
         .Define("C", "return (3*(EigenVals[0]*EigenVals[1]+EigenVals[0]*EigenVals[2]+EigenVals[1]*EigenVals[2]));") \
@@ -84,64 +99,28 @@ for fname in fnames:
         .Define("LambdaMax", "int max = 0; for (int i = 0; i < 3; i++)if (EigenVals[i] > max) max = EigenVals[i]; return max") \
         .Define("EigenVals2",eigenString2) \
         .Define("Sphericity", "return (EigenVals2 [1] + EigenVals2 [2])*3/2")# the nTracks cut is probably unnecessary
-    #print(filteredFrames[mass].Count().GetValue())
-    print("d4")
+
     models[mass+"C"] = ROOT.RDF.TH1DModel("C"+mass, mass, 50, 0., 1.)
     cHists[mass] = filteredFrames[mass].Histo1D(models[mass+"C"], "C").Clone("cloneC"+mass)
-    models[mass + "D"] = ROOT.RDF.TH1DModel("D" + mass, mass, 50, 0., 1.)
-    dHists[mass] = filteredFrames[mass].Histo1D(models[mass + "D"], "D").Clone("cloneD"+mass)
-    models[mass + "L"] = ROOT.RDF.TH1DModel("L" + mass, mass, 50, 0., 1.)
-    lHists[mass] = filteredFrames[mass].Histo1D(models[mass + "L"], "LambdaMax").Clone("cloneL" + mass)
-    models[mass + "S"] = ROOT.RDF.TH1DModel("S" + mass, mass, 50, 0., 1.)
-    sHists[mass] = filteredFrames[mass].Histo1D(models[mass + "S"], "Sphericity").Clone("cloneS" + mass)
 
-    print("d5")
-print("e")
-
-
-print("f")
-print(cHists.keys())
-
-#copied from plotHelpers:
 can = ROOT.TCanvas("canName", "canTitle")
-print("g")
 file = ROOT.TFile('hists', 'RECREATE')
-print("g1")
 cHists["mMed-125"].Write()
-print("g2")
-'''
-i = 0
-more_colors = [ROOT.kAzure+2, ROOT.kGreen+2, ROOT.kPink+4, ROOT.kOrange+10, ROOT.kOrange, ROOT.kSpring+7]
-print("h")
-for mass in cHists.keys():
-    print("i")
-    cHists[mass].SetLineColor(more_colors[i])
-    print("j")
-    cHists[mass].SetMarkerColor(more_colors[i])
-    print("k")
-    i += 1
-    print("l")
-    i %= len(more_colors)'''
 
 cHists["mMed-125"].SetLineColor(2)
 cHists["mMed-400"].SetLineColor(3)
 cHists["mMed-750"].SetLineColor(4)
 cHists["mMed-1000"].SetLineColor(6)
-print("m")
 
-#cHists["mMed-125"].SetMinimum(0)
-#cHists["mMed-125"].SetMaximum(1)
-cHists["mMed-125"].Draw("hist")
+cHists["mMed-1000"].Draw("hist")
+cHists["mMed-125"].Draw("same")
 cHists["mMed-400"].Draw("same")
 cHists["mMed-750"].Draw("same")
-cHists["mMed-1000"].Draw("same")
-
-print("n")
 
 leg = ROOT.TLegend(.66, .64, .8, .88)
-print("o")
+
 for mass in cHists.keys():
     leg.AddEntry(cHists[mass], mass, "l")
 leg.Draw()
-print("p")
-can.SaveAs("shapesPlot.pdf")
+
+can.SaveAs("cPlot.pdf")
